e8f0d3060a1c22105b63746841ab11a8476a364d
superman <goodnesskolapo@gmail.com>
2023-02-24 20:39:11 +0100

    started task 2

---
+++ contracts/highestusdtholder.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+
+interface IERC20 {
+    function balanceOf(address account) external view returns (uint256);
+}
+contract gethigestholder {
+    constructor() {
+        
+    }
+}
