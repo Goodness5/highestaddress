2379ef7f0a0f4086478c043d3bb89652c4f508d2
superman <goodnesskolapo@gmail.com>
2023-02-25 21:47:04 +0100

    this

--- scripts/highestholder.ts
+++ scripts/highestholder.ts
@@ -1,16 +1,17 @@
 import { ethers } from 'ethers';
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 
 dotenv.config();
-async function getHighestHolder(){
 
-const provider = new ethers.providers.JsonRpcProvider(process.env.MAINETURL);
-const contractAddress = '0xdAC17F958D2ee523a2206206994597C13D831ec7';
-
-
+async function getHighestHolder() {
+  const response = await fetch(
+    'https://api.ethplorer.io/getTopTokenHolders/0xdAC17F958D2ee523a2206206994597C13D831ec7?apiKey=freekey&limit=1'
+  );
+  const data = await response.json();
+  console.log(data);
 }
 
 getHighestHolder().catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
-  });
+  console.error(error);
+  process.exitCode = 1;
+});
